ea83942d124e5511081f84f999402026bac9eb19
laurijssen <lauserve@hotmail.com>
2021-06-10 11:16:24 +0200

    initial commit with login screen

---
+++ Geocachefriends/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Geocachefriends.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Geocachefriends
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var vm = new LoginViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+
+            InitializeComponent();
+
+            Email.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                vm.SubmitCommand.Execute(null);
+            };
+        }
+    }
+}
